242f2433accc2cf9ee52daca4ce8c1bac2e27096
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-08-31 04:19:52 +0900

    Time: 770 ms (64.05%), Space: 84.4 MB (56.27%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        q = collections.deque()
+        
+        if not head:
+            return True
+        
+        node = head
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+            
+        while len(q)>1:
+            if q.popleft() != q.pop():
+                return False
+        
+        return True
+        
