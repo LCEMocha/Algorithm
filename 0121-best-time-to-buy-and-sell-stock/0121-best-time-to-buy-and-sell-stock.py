8d6829807e6e31b2521645759b8afd91d0adb836
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-08-30 23:18:25 +0900

    Time: 785 ms (64.87%), Space: 22.8 MB (7.84%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        profit = 0
+        min_price = sys.maxsize
+        
+        #최솟값과 최댓값을 계속 갱신
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = max(profit, price - min_price)
+            
+        return profit
+            
