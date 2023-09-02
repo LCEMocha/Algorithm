9a6b0c77adc22013f5e4b58aa578e91ee8dd8283
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-09-02 22:15:39 +0900

    Time: 535 ms (98.81%), Space: 49.7 MB (97.00%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,16 +5,18 @@
 #         self.next = next
 class Solution(object):
     def isPalindrome(self, head):
-        q = collections.deque()
-
-        node = head
-        while node is not None:
-            q.append(node.val)
-            node = node.next
-            
-        while len(q)>1:
-            if q.popleft() != q.pop():
-                return False
+        rev = None
+        slow = fast = head
         
-        return True
+        #런너를 이용해 역순 연결리스트 구성
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+            
+        #팰린드롬 여부 확인
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev
         
