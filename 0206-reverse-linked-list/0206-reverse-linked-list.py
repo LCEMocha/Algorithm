8be843e098572d61b5ff0974ac75782664d3fdeb
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-09-03 23:34:08 +0900

    Time: 21 ms (50.26%), Space: 15.2 MB (56.17%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -1,13 +1,11 @@
 class Solution(object):
     def reverseList(self, head):
-        prev = None
-        curr = head
-        while curr is not None:
-            next_node = curr.next  # 현재 노드의 다음 노드를 임시로 저장
-            curr.next = prev       # 현재 노드의 next를 이전 노드로 바꾸기
-            prev = curr            # 이전 노드를 현재 노드로 업데이트
-            curr = next_node       # 다음 노드로 이동
-
+        node, prev = head, None
+        
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
+        
         return prev
 
 
