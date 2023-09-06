4fd517a91969fbbac9610cf5af40d6310e252456
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-09-07 03:01:38 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,27 +1,30 @@
 def solution(sequence, k):
-    start, end = 0, 0
-    current_sum = sequence[0]
-    result = []
+    if k in sequence:
+        return [sequence.index(k), sequence.index(k)]
 
-    while end < len(sequence):
+    start, end = 0, 1
+    current_sum = sequence[start] + sequence[end]
+    result = [start, len(sequence) - 1]
+
+    while end < len(sequence) and start < len(sequence):
         if current_sum == k:
-            # 현재 부분 수열의 길이를 확인하고, result를 업데이트
-            if not result or (end - start) < (result[1] - result[0]):
+            if (result[1] - result[0]) > (end - start):
                 result = [start, end]
-
-            # 시작 인덱스를 증가시켜서 다른 부분 수열도 확인
+            
+            # After updating the result, move the start pointer to adjust the sum
             current_sum -= sequence[start]
             start += 1
 
         elif current_sum < k:
             end += 1
-            if end < len(sequence):
+            if end < len(sequence):  # Avoid IndexError
                 current_sum += sequence[end]
-        else:
+        else:  # current_sum > k
             current_sum -= sequence[start]
             start += 1
-            if start > end and start < len(sequence):
-                end = start
-                current_sum = sequence[end]
-                
+
+            if start == end and end < len(sequence) - 1: 
+                end += 1
+                current_sum += sequence[end]
+
     return result
