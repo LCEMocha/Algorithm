c548ca7f932a70b329a6681862b480634277e492
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-06 22:39:27 +0900

    Time: 52 ms (89.68%), Space: 16.3 MB (65.96%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,12 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        find = []
-        right = 1
-        if not s:
-            return 0
-        for i in range(len(s)):
-            while right+1 <= len(s) and len(set(s[i:right+1])) == len(s[i:right+1]): 
-                right += 1
-            find.append(s[i:right])    
-            right = i+1
-        return len(max(find, key=len))
+        used = {}
+        max_length = start = 0
+        for index, char in enumerate(s):
+            # 이미 등장했던 문자라면 start 위치 갱신
+            if char in used and start <= used[char]:
+                start = used[char] + 1
+            else:  # 최대 부분 문자열 길이 갱신
+                max_length = max(max_length, index-start+1)
+            
+            # 현재 문자의 위치 삽입
+            used[char] = index
+        
+        return max_length
