361f8d10dd4bf74ff6260667320f71f74ec598c0
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-06 20:19:38 +0900

    Time: 45 ms (15.17%), Space: 16.2 MB (80.21%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,7 +1,3 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        s = list(jewels)
-        answer = 0
-        for i in s:
-            answer += stones.count(i)
-        return answer
+        return sum(s in jewels for s in stones)
