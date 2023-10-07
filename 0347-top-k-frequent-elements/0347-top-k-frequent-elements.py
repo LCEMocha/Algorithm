a1f63b171e779902e7c8416f0ca21493411697f4
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-07 19:01:51 +0900

    Time: 99 ms (58.34%), Space: 20.6 MB (99.77%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,8 +1,3 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        c = collections.Counter(nums)
-        common = c.most_common(k)
-        answer = []
-        for i in common:
-            answer.append(i[0])
-        return answer
+        return list(zip(*collections.Counter(nums).most_common(k)))[0]
