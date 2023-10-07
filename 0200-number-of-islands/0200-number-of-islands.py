9a5bcd2f1b05174ee5e33075a4539fe4dfd5161e
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-07 21:16:33 +0900

    Time: 285 ms (65.35%), Space: 18.7 MB (84.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,7 +2,8 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def dfs(i,j):
             # 더 이상 땅이 아닌 경우 종료
-            if (i<0 or i>=len(grid)) or (j<0 or j>=len(grid[0])) or \
+            if (i<0 or i>=len(grid)) or \
+            (j<0 or j>=len(grid[0])) or \
             (grid[i][j] != '1'):
                 return
         
