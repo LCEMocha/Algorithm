ad3a3d04ad8f33250e41af6d598c5715d1942a15
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-09 21:18:22 +0900

    Time: 41 ms (39.24%), Space: 16.1 MB (99.12%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -9,6 +9,10 @@ class Solution:
               '5':['j','k','l'], '6':['m','n','o'], '7':['p','q','r','s'],
               '8':['t','u','v'], '9':['w','x','y','z']}
         
-        lookup = [dic[digit] for digit in digits] 
-        answer = [''.join(combination) for combination in itertools.product(*lookup)]
-        return answer
+        # digits의 각 숫자에 해당하는 문자 리스트를 lookup
+        lookup = [dic[digit] for digit in digits]
+        
+        # product를 사용해 모든 조합 구하기
+        combinations = [''.join(combination) for combination in itertools.product(*lookup)]
+        
+        return combinations
