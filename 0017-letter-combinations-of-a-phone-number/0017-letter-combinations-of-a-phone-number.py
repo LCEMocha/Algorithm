eb1dd3a9fba2f8dfc1e742ae950fddbf6e01fa43
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-09 21:24:53 +0900

    Time: 38 ms (57.34%), Space: 16.4 MB (38.95%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,17 +2,26 @@ import itertools
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits: 
+        def dfs(index, path):
+            # 끝까지 탐색하면 백트래킹
+            if len(path) == len(digits):
+                result.append(path)
+                return
+        
+            # 입력값 자릿수 단위 반복
+            for i in range(index, len(digits)):
+                # 숫자에 해당하는 모든 문자열 반복
+                for j in dic[digits[i]]:
+                    dfs(i+1, path+j)   
+        
+        # 예외처리
+        if not digits:
             return []
         
         dic = {'2':['a','b','c'], '3':['d','e','f'], '4':['g','h','i'],
               '5':['j','k','l'], '6':['m','n','o'], '7':['p','q','r','s'],
               '8':['t','u','v'], '9':['w','x','y','z']}
+        result = []
+        dfs(0,"")
         
-        # digits의 각 숫자에 해당하는 문자 리스트를 lookup
-        lookup = [dic[digit] for digit in digits]
-        
-        # product를 사용해 모든 조합 구하기
-        combinations = [''.join(combination) for combination in itertools.product(*lookup)]
-        
-        return combinations
+        return result
