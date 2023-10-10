32df1b0754b85691000e4509ea4d86b1a0a26837
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-10 23:59:08 +0900

    Time: 42 ms (76.59%), Space: 16.3 MB (95.49%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,26 @@
-from itertools import permutations
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  # 중복 처리를 위해 먼저 정렬
         results = []
         prev_elements = []
-        
+
         def dfs(elements):
-            # 리프 노드일 때 결과 추가
-            if len(elements) == 0:
+            if not elements:  # 원소가 없을 때 결과 추가
                 results.append(prev_elements[:])
-                
-            # 순열 생성 재귀 호출
-            for e in elements:
-                next_elements = elements[:]
-                next_elements.remove(e)
-                
-                prev_elements.append(e)
+                return
+
+            prev = None  # 중복 처리를 위한 변수
+            for i in range(len(elements)):
+                if prev == elements[i]:  # 중복된 값은 skip
+                    continue
+
+                next_elements = elements[:i] + elements[i+1:]
+                prev_elements.append(elements[i])
                 dfs(next_elements)
                 prev_elements.pop()
-                
+
+                prev = elements[i]  # 현재 원소 저장
+
         dfs(nums)
         return results
