6a172bde6e3365428820751c8ad9281aa9df37f6
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-10 22:30:44 +0900

    Time: 1619 ms (14.31%), Space: 59.6 MB (6.55%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,9 @@
+from itertools import permutations
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        li = []
+        for i in range(n):
+            li.append(i+1)
+        result = list(permutations(li,n))
+        result = ''.join(map(str,result[k-1]))
+        return result
