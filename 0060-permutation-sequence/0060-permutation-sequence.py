fed198cc432d21b8750e35d00330a577c11d88da
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-14 22:29:32 +0900

    Time: 1909 ms (11.31%), Space: 59.7 MB (6.35%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,18 +1,27 @@
 class Solution:
+    def permutation(self, data):
+        n = len(data)
+        c = [0] * n
+        yield data[:]
+
+        i = 0
+        while i < n:
+            if c[i] < i:
+                if i % 2 == 0:
+                    data[0], data[i] = data[i], data[0]
+                else:
+                    data[c[i]], data[i] = data[i], data[c[i]]
+                yield data[:]
+                c[i] += 1
+                i = 0
+            else:
+                c[i] = 0
+                i += 1
+    
     def getPermutation(self, n: int, k: int) -> str:
-        # 팩토리얼 값을 계산하는 도우미 함수
-        def factorial(num):
-            if num == 0:
-                return 1
-            return num * factorial(num - 1)
-        
-        numbers = list(range(1, n+1))
-        k -= 1  # 인덱스는 0부터 시작하므로 1을 빼준다.
-        res = []
-        
-        while n > 0:
-            n -= 1
-            index, k = divmod(k, factorial(n))
-            res.append(numbers.pop(index))
-        
-        return ''.join(map(str, res))
+        iterable = []
+        for i in range(1, n+1):
+            iterable.append(i)
+        result = list(permutations(iterable))
+        result.sort()
+        return ''.join(map(str,result[k-1]))
