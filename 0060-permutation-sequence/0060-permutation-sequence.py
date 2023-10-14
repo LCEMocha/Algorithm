b58a6cd17c353211e91d4c3f62430a25e99f6cd4
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-14 22:08:09 +0900

    Time: 39 ms (61.64%), Space: 16.1 MB (98.29%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,23 +1,4 @@
 class Solution:
-    def permutation(self, data):
-        n = len(data)
-        c = [0] * n
-        yield data[:]
-
-        i = 0
-        while i < n:
-            if c[i] < i:
-                if i % 2 == 0:
-                    data[0], data[i] = data[i], data[0]
-                else:
-                    data[c[i]], data[i] = data[i], data[c[i]]
-                yield data[:]
-                c[i] += 1
-                i = 0
-            else:
-                c[i] = 0
-                i += 1
-    
     def getPermutation(self, n: int, k: int) -> str:
         # 팩토리얼 값을 계산하는 도우미 함수
         def factorial(num):
