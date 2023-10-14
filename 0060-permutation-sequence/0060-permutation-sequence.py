e39b01f430f9277b2a618e3bf5a438a1ae33fdf2
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-14 21:24:01 +0900

    Time: 42 ms (47.63%), Space: 16.2 MB (66.28%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -19,12 +19,19 @@ class Solution:
                 i += 1
     
     def getPermutation(self, n: int, k: int) -> str:
-        iterable = []
-        for i in range(1, n+1):
-            iterable.append(i)
-        result = []
-        for p in permutations(iterable):
-            result.append(p)
-        result.sort()
-        return ''.join(map(str,result[k-1]))
-    
+        # 팩토리얼 값을 계산하는 도우미 함수
+        def factorial(num):
+            if num == 0:
+                return 1
+            return num * factorial(num - 1)
+        
+        numbers = list(range(1, n+1))
+        k -= 1  # 인덱스는 0부터 시작하므로 1을 빼준다.
+        res = []
+        
+        while n > 0:
+            n -= 1
+            index, k = divmod(k, factorial(n))
+            res.append(numbers.pop(index))
+        
+        return ''.join(map(str, res))
