36d9cf10fa21a17dbe707385909bc42e6031942e
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-14 21:18:19 +0900

    Time: 2248 ms (8.94%), Space: 59.5 MB (6.35%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,8 +1,30 @@
-from itertools import permutations
 class Solution:
+    def permutation(self, data):
+        n = len(data)
+        c = [0] * n
+        yield data[:]
+
+        i = 0
+        while i < n:
+            if c[i] < i:
+                if i % 2 == 0:
+                    data[0], data[i] = data[i], data[0]
+                else:
+                    data[c[i]], data[i] = data[i], data[c[i]]
+                yield data[:]
+                c[i] += 1
+                i = 0
+            else:
+                c[i] = 0
+                i += 1
+    
     def getPermutation(self, n: int, k: int) -> str:
-        li = []
-        for i in range(n):
-            li.append(i+1)
-        result = list(permutations(li,n))
+        iterable = []
+        for i in range(1, n+1):
+            iterable.append(i)
+        result = []
+        for p in permutations(iterable):
+            result.append(p)
+        result.sort()
         return ''.join(map(str,result[k-1]))
+    
