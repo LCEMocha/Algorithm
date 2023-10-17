826a40615ca3e6b503ead9ebe48750fcc4c76aa3
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-17 23:00:10 +0900

    Time: 905 ms (50.23%), Space: 62 MB (95.60%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,4 +1,27 @@
 from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return list(itertools.combinations(range(1,n+1),k))
+        return list(self.combi(range(1,n+1), k))
+        
+    def combi(self, iterable, r):
+        pool = tuple(iterable)
+        l = len(pool)
+
+        if r > l:
+            return
+
+        indices = list(range(r))
+        yield tuple(pool[i] for i in indices)
+
+        while True:
+            for i in reversed(range(r)):
+                if indices[i] != i + l - r:
+                    break
+            else:
+                return
+
+            indices[i] += 1
+            for j in range(i + 1, r):
+                indices[j] = indices[j-1] + 1
+
+            yield tuple(pool[i] for i in indices)
