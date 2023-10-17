5a29b43498295b5e14dda236a9d84d30e8c08a12
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-17 23:11:02 +0900

    Time: 929 ms (31.33%), Space: 65.4 MB (10.94%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,17 +1,16 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        results = []
-        
-        def dfs(elements, start, k):
-            if k == 0:
-                results.append(elements[:])
-                return
-            
-            # 자신 이전의 모든 값을 고정하여 재귀 호출
-            for i in range(start, n+1):
-                elements.append(i)
-                dfs(elements, i+1, k-1)
-                elements.pop()
-            
-        dfs([], 1, k)
-        return results
+        def dfs(start=1, path=[]):
+
+            if len(path) == k:
+                return [path[:]]
+            if start > n:
+                return []
+
+            with_current = dfs(start + 1, path + [start])
+
+            without_current = dfs(start + 1, path)
+
+            return with_current + without_current
+
+        return dfs()
