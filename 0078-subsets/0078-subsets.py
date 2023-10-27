3080e469070438236f873e55437a5a9b6fe259f4
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-10-27 18:58:04 +0900

    Time: 44 ms (31.35%), Space: 16.5 MB (54.23%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,6 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
+        
+        def dfs(elements, start, k):
+            if k == 0:
+                result.append(elements[:])
+                return
+
+            for i in range(start, len(nums)):
+                elements.append(nums[i])  # 트리에서 깊어지는 부분
+                dfs(elements, i+1, k-1)
+                elements.pop()  # 트리에서 거꾸로 올라오는 부분
+
+                
         for i in range(len(nums)+1):
-            result = result+list(combinations(nums,i))  
+            dfs([], 0, i)
         return result
