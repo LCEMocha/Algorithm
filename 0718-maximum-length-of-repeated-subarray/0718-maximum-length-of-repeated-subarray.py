ca2c202d584cf98b03bc3361311dd2e154f70beb
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-11-10 23:21:46 +0900

    Time: 1941 ms (43.08%), Space: 41.3 MB (55.07%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -4,11 +4,11 @@ class Solution:
         dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
         max_length = 0
 
-        # 동적 프로그래밍을 이용하여 최장 공통 부분 배열의 길이 계산
-        for i in range(len(nums1) - 1, -1, -1):
-            for j in range(len(nums2) - 1, -1, -1):
-                if nums1[i] == nums2[j]:
-                    dp[i][j] = dp[i + 1][j + 1] + 1
+        # 동적 프로그래밍을 이용하여 최장 공통 부분 배열의 길이 계산 (순차적 접근)
+        for i in range(1, len(nums1) + 1):
+            for j in range(1, len(nums2) + 1):
+                if nums1[i - 1] == nums2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
                     max_length = max(max_length, dp[i][j])
 
         return max_length        
