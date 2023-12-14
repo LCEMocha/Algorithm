730c82f0a320be5b5cc57f4218ed1bda9cd5a8f1
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-12-15 02:25:06 +0900

    Time: 288 ms (18.14%), Space: 20.3 MB (65.09%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.py
+++ 0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -9,22 +9,30 @@ class Solution:
         self.longest = 0
 
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
-        def dfs(node, parent_val):
+        def dfs(node):
             if not node:
                 return 0
 
-            left_len = dfs(node.left, node.val)
-            right_len = dfs(node.right, node.val)
+            # 왼쪽과 오른쪽 자식 노드에서 시작하는 유니밸류 경로의 길이를 계산합니다.
+            left_len = dfs(node.left)
+            right_len = dfs(node.right)
 
-            self.longest = max(self.longest, left_len + right_len)
+            # 왼쪽 및 오른쪽 경로의 길이를 0으로 초기화합니다.
+            left_path, right_path = 0, 0
 
-            # 현재 노드의 값이 부모 노드의 값과 같은 경우,
-            # 왼쪽 또는 오른쪽 자식 중 긴 경로를 선택하고 현재 노드를 포함(길이 +1).
-            if node.val == parent_val:
-                return max(left_len, right_len) + 1
+            # 현재 노드의 값이 왼쪽 자식 노드의 값과 같으면, 왼쪽 경로 길이를 업데이트합니다.
+            if node.left and node.left.val == node.val:
+                left_path = left_len + 1
 
-            # 현재 노드의 값이 부모 노드의 값과 다른 경우, 새 경로를 시작해야 하므로 0을 반환합니다.
-            return 0
+            # 현재 노드의 값이 오른쪽 자식 노드의 값과 같으면, 오른쪽 경로 길이를 업데이트합니다.
+            if node.right and node.right.val == node.val:
+                right_path = right_len + 1
 
-        dfs(root, None)
+            # 최대 유니밸류 경로 길이를 업데이트합니다.
+            self.longest = max(self.longest, left_path + right_path)
+
+            # 현재 노드에서 끝나는 최장 유니밸류 경로의 길이를 반환합니다.
+            return max(left_path, right_path)
+
+        dfs(root)
         return self.longest
