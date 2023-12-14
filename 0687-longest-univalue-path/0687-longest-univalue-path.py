46d2297433711952e37b0214d7cb06703e2287c4
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-12-15 02:02:41 +0900

    Time: 280 ms (26.68%), Space: 21.1 MB (13.11%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.py
+++ 0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -9,15 +9,28 @@ class Solution:
         self.longest = 0
 
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
-        def dfs(node, parent_val):
-            if not node:
+        def dfs(node):
+            if node is None:
                 return 0
-            left_len = dfs(node.left, node.val)
-            right_len = dfs(node.right, node.val)
-            self.longest = max(self.longest, left_len + right_len)
-            if node.val == parent_val:
-                return max(left_len, right_len) + 1
-            return 0
-
-        dfs(root, None)
+            
+            # 존재하지 않는 노드까지 DFS 재귀 탐색
+            left = dfs(node.left)
+            right = dfs(node.right)
+            
+            # 현재 노드가 자식 노드와 동일한 경우 거리 1증가
+            if node.left and node.left.val == node.val:
+                left += 1
+            else:
+                left = 0
+            if node.right and node.right.val == node.val:
+                right += 1
+            else:
+                right = 0
+                
+            # 왼쪽과 오른쪽 자식 노드 간 거리의 합 최댓값이 결과
+            self.longest = max(self.longest, left+right)
+            # 자식 노드 상태값 중 큰 값 리턴
+            return max(left, right)
+    
+        dfs(root)
         return self.longest
