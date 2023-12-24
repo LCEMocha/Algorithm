3c2b521d8e4e5726c437329dacc3ce7f9ed97b8d
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2023-12-25 08:31:47 +0900

    Time: 116 ms (98.49%), Space: 24.6 MB (95.54%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -10,10 +10,8 @@ class Solution:
         if root:
             if low <= root.val <= high:
                 self.sum += root.val
-            if low < root.val:  # 범위 내에 있을 수 있는 왼쪽 서브트리 탐색
-                self.rangeSumBST(root.left, low, high)
-            if high > root.val:  # 범위 내에 있을 수 있는 오른쪽 서브트리 탐색
-                self.rangeSumBST(root.right, low, high)
+            self.rangeSumBST(root.left, low, high)
+            self.rangeSumBST(root.right, low, high)
         return self.sum
 
         
