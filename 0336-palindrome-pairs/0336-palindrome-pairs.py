73fad7ae4f3cec11b47dc7bdc7108a14c3578ca5
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-01-02 07:14:40 +0900

    Time: 4204 ms (10.15%), Space: 29.3 MB (65.56%) - LeetHub

--- 0336-palindrome-pairs/0336-palindrome-pairs.py
+++ 0336-palindrome-pairs/0336-palindrome-pairs.py
@@ -1,54 +1,22 @@
-class TrieNode:
-    def __init__(self):
-        self.children = collections.defaultdict(TrieNode)
-        self.word_id = -1
-        self.palindrome_word_ids = []
-        
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-        
-    @staticmethod
-    def is_palindrome(word):
-        return word == word[::-1] 
-    
-    # 단어 삽입
-    def insert(self, index, word):
-        node = self.root
-        for i, char in enumerate(reversed(word)):
-            if self.is_palindrome(word[0:len(word) - i]):
-                node.palindrome_word_ids.append(index)
-            node = node.children[char]
-        node.word_id = index
-    
-    def search(self, index, word):
-        result = []
-        node = self.root
-        
-        while word:
-            if node.word_id >= 0:
-                if self.is_palindrome(word):
-                    result.append([index, node.word_id])
-            if word[0] not in node.children:
-                return result
-            node = node.children[word[0]]
-            word = word[1:]
-        
-        if node.word_id >= 0 and node.word_id != index:
-            result.append([index, node.word_id])
-        for palindrome_word_id in node.palindrome_word_ids:
-            result.append([index, palindrome_word_id])
-
-        return result 
-        
 class Solution:
-    def palindromePairs(self, words: List[str]) -> List[List[int]]:
-        trie = Trie()
-        
-        for i, word in enumerate(words):
-            trie.insert(i, word)
-            
-        results = []
-        for i, word in enumerate(words):
-            results.extend(trie.search(i, word))
-        return results
+    def palindromePairs(self, words):
+        def is_palindrome(check):
+            return check == check[::-1]
+
+        words = {word: i for i, word in enumerate(words)}
+        valid_pals = []
+
+        for word, k in words.items():
+            n = len(word)
+            for j in range(n + 1):
+                pref = word[:j]
+                suf = word[j:]
+                if is_palindrome(pref):
+                    back = suf[::-1]
+                    if back != word and back in words:
+                        valid_pals.append([words[back], k])
+                if j != n and is_palindrome(suf):
+                    back = pref[::-1]
+                    if back != word and back in words:
+                        valid_pals.append([k, words[back]])
+        return valid_pals
