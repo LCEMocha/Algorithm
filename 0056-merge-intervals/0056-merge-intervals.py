f14af6f9c41c0cd517a4b154f0b205c62b281706
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-01-14 02:38:34 +0900

    Time: 122 ms (89.97%), Space: 21.5 MB (20.66%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,18 +1,10 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort()
         m = []
-        s = intervals[0][0]
-        e = intervals[0][1]
-        for i, j in intervals:
-            if s<=i and i<=e:
-                if j<e:
-                    continue
-                e=j
+        for i in sorted(intervals, key=lambda x:x[0]):
+            if m and i[0] <= m[-1][1]:
+                m[-1][1] = max(m[-1][1],i[1])
             else:
-                m.append([s,e])
-                s=i
-                e=j
-        m.append([s,e])
+                # 콤마는 중첩 리스트로 만들어주는 역할을 한다. 그냥 += 만 하면 단순히 요소를 이어붙여줌 
+                m += i,
         return m
-                
