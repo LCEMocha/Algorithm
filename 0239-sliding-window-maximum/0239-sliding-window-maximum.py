d5a5c98d803c9d5c604b54378a515e65a49b42d1
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-01-27 05:38:28 +0900

    Time: 1105 ms (82.33%), Space: 32.3 MB (95.38%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,19 +1,19 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+
         if not nums or k == 0:
             return []
 
-        # 이중 종단 큐 초기화
         dq = deque()
         result = []
 
-        for i in range(len(nums)):
+        for i, num in enumerate(nums):
             # 윈도우의 왼쪽 끝이 큐의 첫 번째 요소보다 커지면 해당 요소를 제거
             if dq and dq[0] < i - k + 1:
                 dq.popleft()
 
             # 현재 요소가 큐의 마지막 요소보다 크거나 같을 때까지 큐의 마지막 요소 제거
-            while dq and nums[i] >= nums[dq[-1]]:
+            while dq and nums[dq[-1]] <= num:
                 dq.pop()
 
             # 현재 인덱스 추가
@@ -24,3 +24,4 @@ class Solution:
                 result.append(nums[dq[0]])
 
         return result
+
