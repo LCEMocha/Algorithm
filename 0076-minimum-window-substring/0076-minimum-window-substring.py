ef264d6e5c8d4151257456eebe854d017191d609
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-01-28 00:28:31 +0900

    Time: 98 ms (75.19%), Space: 17.3 MB (61.71%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,33 +1,24 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not t or not s:
-            return ""
-
-        dict_t = Counter(t)
-        required = len(dict_t)
-
-        l, r = 0, 0
-
-        formed = 0
-
-        window_counts = {}
-
-        ans = float("inf"), None, None
-
-        while r < len(s):
-            character = s[r]
-            window_counts[character] = window_counts.get(character, 0) + 1
-            if character in dict_t and window_counts[character] == dict_t[character]:
-                formed += 1
-
-            while l <= r and formed == required:
-                character = s[l]
-                if r - l + 1 < ans[0]:
-                    ans = (r - l + 1, l, r)
-                window_counts[character] -= 1
-                if character in dict_t and window_counts[character] < dict_t[character]:
-                    formed -= 1
-
-                l += 1    
-            r += 1    
-        return "" if ans[0] == float("inf") else s[ans[1]:ans[2] + 1]
+        need = collections.Counter(t)
+        missing = len(t)
+        left = start = end = 0
+        
+        # 오른쪽 포인터 이동
+        for right, char in enumerate(s, 1):
+            missing -= need[char] > 0
+            need[char] -= 1
+            
+            # 필요 문자가 0이면 왼쪽 포인터 이동 판단
+            if missing == 0:
+                while left < right and need[s[left]] < 0:
+                    need[s[left]] += 1
+                    left += 1
+                
+                if not end or right - left <= end - start:
+                    start, end = left, right
+                    need[s[left]] += 1
+                    missing += 1
+                    left += 1
+                    
+        return s[start:end]
