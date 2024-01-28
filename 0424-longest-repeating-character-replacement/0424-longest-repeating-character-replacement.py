7ed1d98741814f1708be0690ec2c4a67ee69994e
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-01-29 03:20:04 +0900

    Time: 108 ms (51.61%), Space: 16.7 MB (63.18%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,21 +1,18 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        hasho = {}
+        count = Counter()
         most_frequent = 0
         start = 0
         max_len = 0
 
         for end, char in enumerate(s):
-            
-            hasho[char] = hasho.get(char, 0) + 1 
-            
-            most_frequent = max(most_frequent, hasho[char])
-            # (end - start + 1) --> window size 
+            count[char] += 1
+            most_frequent = max(most_frequent, count[char])
+
             if (end - start + 1) - most_frequent > k:
-                hasho[s[start]] -= 1
+                count[s[start]] -= 1
                 start += 1
-            
-            max_len = max(max_len, end-start+1)
-        
+
+            max_len = max(max_len, end - start + 1)
+
         return max_len
-        
