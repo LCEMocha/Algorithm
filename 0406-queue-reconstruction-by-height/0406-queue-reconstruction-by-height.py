7ceb70a7cf3823c7fea5c20f6b414a529adef2ee
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-01-29 06:27:59 +0900

    Time: 72 ms (98.93%), Space: 17.1 MB (77.65%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
@@ -1,14 +1,12 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        heap = []
-        # 키 역순, 인덱스 삽입
-        for person in people:
-            heapq.heappush(heap, (-person[0], person[1]))
-            
+        # 키에 대해 내림차순, 같은 키 내에서는 앞에 서야 할 사람 수에 대해 오름차순으로 정렬
+        people.sort(key=lambda x: (-x[0], x[1]))
+        
         result = []
-        # 키 역순, 인덱스 추출
-        while heap:
-            person = heapq.heappop(heap)
-            result.insert(person[1], [-person[0], person[1]])
+        # 정렬된 순서대로 각 사람을 적절한 위치에 삽입
+        for person in people:
+            result.insert(person[1], person)
+        
         return result
         
