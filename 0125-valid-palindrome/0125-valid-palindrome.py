ac7e4e2138f97cc586845380f672694dc638feff
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-02-16 02:59:40 +0900

    Time: 43 ms (73.53%), Space: 18.3 MB (23.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,8 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        
-        s = ''.join(e for e in s if e.isalnum()).lower()
-
-        return s == s[::-1]
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub(r"[^\uAC00-\uD7A30-9a-zA-Z\s]", "", s)
+        s = ''.join(s.split())
+        if s == s[::-1]:
+            return True
+        return False
