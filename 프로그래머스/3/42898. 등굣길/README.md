7f0779b26695de8cc6b0ce361f2a02eedfd704d4
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-03-23 02:11:16 +0900

    [level 3] Title: 등굣길, Time: 3.49 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 3.08 ms
+메모리: 10.3 MB, 시간: 3.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 23일 02:08:47
+2024년 03월 23일 02:11:14
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,18 +1,18 @@
 def solution(m, n, puddles):
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
-    dp[1][1] = 1  # 시작점
-    for puddle in puddles:
-        dp[puddle[1]][puddle[0]] = -1  # 물에 잠긴 지역 표시
-
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            if dp[i][j] == -1:  # 물에 잠긴 지역은 건너뜀
-                dp[i][j] = 0
+    matrix = [[0 for i in range(m)] for j in range(n)]
+    for x,y in puddles:
+        matrix[y-1][x-1] = -1
+    matrix[0][0] = 1
+    
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == -1:  # 물에 잠긴 지역은 건너뜀
                 continue
-            if i > 1:
-                dp[i][j] += dp[i-1][j]  # 위쪽에서 오는 경로
-            if j > 1:
-                dp[i][j] += dp[i][j-1]  # 왼쪽에서 오는 경로
-            dp[i][j] %= 1000000007  
+            if i > 0 and matrix[i-1][j] > 0:
+                matrix[i][j] += matrix[i-1][j]  # 위쪽에서 오는 경로
+            if j > 0 and matrix[i][j-1] > 0:
+                matrix[i][j] += matrix[i][j-1]  # 왼쪽에서 오는 경로
+            matrix[i][j] %= 1000000007
 
-    return dp[n][m]
+    return matrix[n-1][m-1]
+        
