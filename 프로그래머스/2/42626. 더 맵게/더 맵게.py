e53a2f3244454660035a4954c817aedc330a8721
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2024-03-28 06:50:40 +0900

    [level 2] Title: 더 맵게, Time: 1916.33 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -3,14 +3,16 @@ import heapq
 def solution(scoville, K):
     heapq.heapify(scoville)
     count = 0
-    while len(scoville) > 1:
-        min1 = heapq.heappop(scoville)
-        if min1 >= K:
+    while len(scoville)>1:
+        m1 = heapq.heappop(scoville)
+        if m1 >= K:
             return count
-        min2 = heapq.heappop(scoville)
-        new_scoville = min1 + (min2 * 2)
-        heapq.heappush(scoville, new_scoville)
+        m2 = heapq.heappop(scoville)
+        new = m1 + (m2*2)
+        heapq.heappush(scoville, new) 
         count += 1
-    if scoville[0] >= K:
+    if heapq.heappop(scoville) >= K:
         return count
     return -1
+    
+    
