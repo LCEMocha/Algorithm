f93a279072885299b67a6ac1c1b65f948705c25b
LCEMocha <142338641+LCEMocha@users.noreply.github.com>
2025-07-20 05:12:40 +0900

    [Silver III] Title: 주유소, Time: 120 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -4,21 +4,23 @@ n = int(input().strip())
 distance = list(map(int, input().split()))
 fees = list(map(int, input().split()))
 total_fee = 0
-
-if n == 1:
-    print(0)
-    sys.exit()
-
 min_fee = min(fees[:-1])
 
-for i,j in enumerate(fees):
-    if i == n-1 :
+p = 0
+while p < n-1:
+    if fees[p] == min_fee:
+        remaining_distance = sum(distance[p:])
+        total_fee += remaining_distance * fees[p]
         break
-    elif j > min_fee :
-        total_fee += distance[i]*j
-    else :
-        remaining_distance = sum(distance[i:])
-        total_fee += remaining_distance * j
-        break
+    elif fees[p] < fees[p + 1]:
+        next_p = p + 1
+        total_fee += distance[p] * fees[p]
+        while fees[p] < fees[next_p] and next_p < n-1:
+            total_fee += distance[next_p] * fees[p]
+            next_p += 1
+        p = next_p
+    else:
+        total_fee += distance[p] * fees[p]
+        p += 1
 
 print(total_fee)
